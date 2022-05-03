322445ce9d60c95c047cd234affd9f677c18ae32
Jonathan <jonathan.wpc@gmail.com>
2022-05-03 19:07:50 -0300

    API documentation created

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const apiRoutes = require('./routes');
 const cors = require('cors');
+const fs = require('fs');
+const swaggerUi = require('swagger-ui-express');
+const path = require('path');
+const cssOptions = require('./swagger-ui/css') 
 
 const app = express();
 
@@ -9,6 +13,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(apiRoutes);
+
+const response = fs.readFileSync(path.join(__dirname, './swagger-ui/swagger.json'));
+const swaggerDocs = JSON.parse(response);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, cssOptions));
 
 app.listen(process.env.PORT, () => {
     console.log("🔥 Servidor rodando");
---
+++ src/swagger-ui/css.js
@@ -0,0 +1,10 @@
+const cssOptions = {
+    customCss: `
+      .topbar {
+        display: none;
+      }
+    `,
+    customSiteTitle: "FinanceBox APIs"
+  }
+
+module.exports = cssOptions;
